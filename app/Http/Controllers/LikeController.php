ea8ae4f9a61e4b3cfd12cef42c5f15cd94bb2500
ArthyTheFox <arthur.labarre06@gmail.com>
2023-03-01 15:42:09 +0100

    like ok

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Like;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function newLike(Request $request)
+    {
+       $like = new Like;
+       $like->idUser = $request['idUser'];
+       $like->idComment = $request['idComment'];
+       $like->idPost = $request['idPost'];  
+       $like->save(); 
+       return $like;
+    }
+
+    public function deleteLike(Request $request)
+    {
+        $like = Like::find($request['id']);
+        #affiche l'utilisateur trouvé sur la page web
+        $like->delete();
+        return $like;
+    } 
+}
