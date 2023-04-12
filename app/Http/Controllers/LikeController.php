6d4cfcaf7df94530735b12df5496cec96feb243c
Gafy33 <armando.houpin@hotmail.fr>
2023-04-12 11:14:39 +0200

    nombre like et comments

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -10,17 +10,23 @@ class LikeController extends Controller
     //TODO : verifier deja like
     public function newLike(Request $request)
     {
-       $like = new Like;
-       $like->idUser = $request['idUser'];
-       $like->idComment = $request['idComment'];
-       $like->idPost = $request['idPost'];  
-       $like->save(); 
-       return $like;
+        $verifLike = Like::where('idUser', $request['idUser'])->where('idPost', $request['idPost'])->first();
+
+        if (!$verifLike) {
+            $like = new Like;
+            $like->idUser = $request['idUser'];
+            $like->idComment = $request['idComment'];
+            $like->idPost = $request['idPost'];
+            $like->save();
+            return $like;
+        } else {
+            return response()->json('L\'user à déja Like');
+        }
     }
 
     public function deleteLike($id)
     {
         $like = Like::find($id)->delete();
         return $like;
-    } 
+    }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Like;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     //TODO : recupere nbre de like & 
     public function index()
     {
-        $posts = Post::select('posts.*', 'users.username', 'users.lastname', 'users.firstname')
+        $posts = Post::select('posts.*', 'users.username', 'users.lastname', 'users.firstname', DB::raw('(SELECT COUNT(*) FROM likes WHERE likes.idPost = posts.id) as nbreLike'), DB::raw('(SELECT likes.id FROM likes WHERE likes.idPost = posts.id AND likes.idUser = 1) as isLike'), DB::raw('(SELECT COUNT(*) FROM comments WHERE comments.idPost = posts.id) as nbreComment'))
             ->join('users', 'users.id', '=', 'posts.userId')
             ->where('isArchived', false)
             ->orderBy('posts.id', 'desc')
@@ -40,7 +41,7 @@ class PostController extends Controller
 
     public function show($id)
     {
-        $post = Post::select('posts.*', 'users.username', 'users.lastname', 'users.firstname', 'likes.nbreLike')
+        $post = Post::select('posts.*', 'users.username', 'users.lastname', 'users.firstname', 'likes.nbreLike', DB::raw('(SELECT likes.id FROM likes WHERE likes.idPost = posts.id AND likes.idUser = 1) as isLike'), DB::raw('(SELECT COUNT(*) FROM comments WHERE comments.idPost = posts.id) as nbreComment'))
         ->join('users', 'users.id', '=', 'posts.userId')
         ->leftJoinSub(
             Like::selectRaw('idPost, COUNT(*) as nbreLike')
